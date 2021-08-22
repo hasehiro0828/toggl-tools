4433f1f58b23cef2b9d20c479598e29607854db9
Hiroto Hasegawa <hiroto.hasegawa.0828@gmail.com>
2021-08-22 17:44:50 +0900

    refactor: csv からデータ取得する部分を分離した

--- main.ts
+++ main.ts
@@ -1,58 +1,11 @@
 /* eslint-disable no-console */
-import fs from "fs";
-import { exit } from "process";
-
 import clipboardy from "clipboardy";
-import papaparse from "papaparse";
 
 import { UNIMPORTANT_PROJECTS } from "@/constants";
-import { Project, TimeEntry, TogglCsvJson } from "@/model";
-import { convertDurationToSeconds, convertSecondsToDuration, createTextFromProject } from "@/utils";
+import { convertSecondsToDuration, createTextFromProject, getProjectsFromCsv } from "@/utils";
 
-const CSV_DIR_PATH = "./csv";
+const { projects: projectArray, filePath } = getProjectsFromCsv();
 
-const direntArray = fs.readdirSync(CSV_DIR_PATH, { withFileTypes: true });
-direntArray.sort((a, b) => (a > b ? 1 : -1));
-const firstDirent = direntArray.find((dirent) => dirent.isFile());
-if (typeof firstDirent === "undefined") {
-  console.log("ファイルが見つかりませんでした");
-  exit(1);
-}
-
-const filePath = `${CSV_DIR_PATH}/${firstDirent.name}`;
-
-const file = fs.readFileSync(filePath);
-const parseResult = papaparse.parse(file.toString(), {
-  header: true,
-  // 最初のヘッダーがシングルクウォートで囲まれるバグの対処
-  // https://github.com/mholt/PapaParse/issues/407
-  // csvの最初に何か変なもの入ってるのかも
-  transformHeader: (header) => header.trim(),
-});
-const togglJsonArray: TogglCsvJson[] = parseResult.data as TogglCsvJson[];
-const filteredTogglJsonArray = togglJsonArray.filter((togglJson) => togglJson.Project !== "");
-
-const projectArray: Project[] = [];
-filteredTogglJsonArray.forEach((togglJson) => {
-  const timeEntry: TimeEntry = {
-    name: togglJson.Title,
-    durationSeconds: convertDurationToSeconds(togglJson.Duration),
-  };
-
-  const sameProject = projectArray.find((project) => project.name === togglJson.Project);
-  if (typeof sameProject === "undefined") {
-    projectArray.push({
-      name: togglJson.Project,
-      durationSeconds: convertDurationToSeconds(togglJson.Duration),
-      timeEntries: [timeEntry],
-    });
-  } else {
-    const timeEntryTotalSeconds = convertDurationToSeconds(togglJson.Duration);
-    const projectTotalSeconds = sameProject.durationSeconds;
-    sameProject.timeEntries.push(timeEntry);
-    sameProject.durationSeconds = timeEntryTotalSeconds + projectTotalSeconds;
-  }
-});
 const totalSeconds = projectArray.map((project) => project.durationSeconds).reduce((sum, elm) => sum + elm);
 
 const unimportantProjects = projectArray.filter((project) => UNIMPORTANT_PROJECTS.includes(project.name));
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,16 @@
+import fs from "fs";
+import { exit } from "process";
+
+import papaparse from "papaparse";
+
 import { NAME_TO_ALIAS_MAP } from "@/constants";
-import { Project } from "@/model";
+import { Project, TimeEntry, TogglCsvJson } from "@/model";
 
 export const convertDurationToSeconds = (duration: string): number => {
   const [hours, minutes, seconds] = duration.split(":").map((str) => parseInt(str, 10));
   return hours * 3600 + minutes * 60 + seconds;
 };
+
 export const convertSecondsToDuration = (_seconds: number): string => {
   const zeroPadding = (num: number): string => `00${num}`.slice(-2);
 
@@ -31,4 +37,54 @@ export const createTextFromProject = (project: Project, totalSeconds: number): s
   });
 
   return text;
+};
+
+export const getProjectsFromCsv = (): { projects: Project[]; filePath: string } => {
+  const CSV_DIR_PATH = "./csv";
+
+  const direntArray = fs.readdirSync(CSV_DIR_PATH, { withFileTypes: true });
+  direntArray.sort((a, b) => (a > b ? 1 : -1));
+  const firstDirent = direntArray.find((dirent) => dirent.isFile());
+  if (typeof firstDirent === "undefined") {
+    // eslint-disable-next-line no-console
+    console.error("ファイルが見つかりませんでした");
+    exit(1);
+  }
+
+  const filePath = `${CSV_DIR_PATH}/${firstDirent.name}`;
+
+  const file = fs.readFileSync(filePath);
+  const parseResult = papaparse.parse(file.toString(), {
+    header: true,
+    // 最初のヘッダーがシングルクウォートで囲まれるバグの対処
+    // https://github.com/mholt/PapaParse/issues/407
+    // csvの最初に何か変なもの入ってるのかも
+    transformHeader: (header) => header.trim(),
+  });
+  const togglJsonArray: TogglCsvJson[] = parseResult.data as TogglCsvJson[];
+  const filteredTogglJsonArray = togglJsonArray.filter((togglJson) => togglJson.Project !== "");
+
+  const projectArray: Project[] = [];
+  filteredTogglJsonArray.forEach((togglJson) => {
+    const timeEntry: TimeEntry = {
+      name: togglJson.Title,
+      durationSeconds: convertDurationToSeconds(togglJson.Duration),
+    };
+
+    const sameProject = projectArray.find((project) => project.name === togglJson.Project);
+    if (typeof sameProject === "undefined") {
+      projectArray.push({
+        name: togglJson.Project,
+        durationSeconds: convertDurationToSeconds(togglJson.Duration),
+        timeEntries: [timeEntry],
+      });
+    } else {
+      const timeEntryTotalSeconds = convertDurationToSeconds(togglJson.Duration);
+      const projectTotalSeconds = sameProject.durationSeconds;
+      sameProject.timeEntries.push(timeEntry);
+      sameProject.durationSeconds = timeEntryTotalSeconds + projectTotalSeconds;
+    }
+  });
+
+  return { projects: projectArray, filePath };
 };
