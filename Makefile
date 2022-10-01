16e4867cb1ecb517b006d84ca80fcbde9d45785c
Hiroto Hasegawa <hiroto.hasegawa.0828@gmail.com>
2022-10-01 16:54:05 +0900

    feat: api を使うバージョンを作成した

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 deps:
 	yarn
 
-run/csv2text: deps
+csv2text/run: deps
 	yarn ts-node -r tsconfig-paths/register src/csv2text/main.ts
+
+makeSummary/run: deps
+	yarn ts-node --files -r tsconfig-paths/register src/makeSummary/main.ts
+
+test: deps
+	yarn test
---
+++ src/makeSummary/main.ts
@@ -0,0 +1,92 @@
+import * as fs from "fs";
+
+import { config } from "dotenv";
+
+import Api from "@/makeSummary/api";
+import { GROUP_SETTINGS, IGNORE_PROJECT_NAMES_ON_CHECKING_STATUS } from "@/makeSummary/constants";
+import {
+  getMondayOfLastWeek,
+  convertDateToString,
+  _convertMsToSpendTime,
+  createTimeText,
+  getTimeEntryTextWithStatus,
+} from "@/makeSummary/utils";
+
+config();
+
+const main = async (): Promise<void> => {
+  const api = new Api(process.env.WORKSPACE_ID, process.env.API_TOKEN);
+
+  const since = getMondayOfLastWeek(new Date());
+  const _until = new Date(since); // eslint-disable-line no-underscore-dangle
+  _until.setDate(_until.getDate() + 6);
+  const until = _until;
+
+  const summary = await api.getSummary(convertDateToString(since), convertDateToString(until));
+
+  let textWithTime = `Total Time: ${_convertMsToSpendTime(summary.total_grand)}\n\n`;
+  let textWithoutTime = "";
+
+  const projectNamesOfGroupSettings = Object.values(GROUP_SETTINGS).flat();
+  const devProjects = summary.data.filter((project) => !projectNamesOfGroupSettings.includes(project.title.project));
+
+  const sumOfProjects = devProjects.map((project) => project.time).reduce((sum, elm) => sum + elm, 0);
+  textWithTime += `## 開発系 の合計時間: ${createTimeText(sumOfProjects, summary.total_grand)}\n`;
+  textWithoutTime += "\n";
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const project of devProjects) {
+    textWithTime += `- ${createTimeText(project.time, summary.total_grand)} ${project.title.project}\n`;
+    textWithoutTime += `- ${project.title.project}\n`;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of project.items) {
+      const text = IGNORE_PROJECT_NAMES_ON_CHECKING_STATUS.includes(project.title.project)
+        ? item.title.time_entry
+        : await getTimeEntryTextWithStatus(item); // eslint-disable-line no-await-in-loop
+      textWithTime += `  - ${createTimeText(item.time, summary.total_grand)} ${text}\n`;
+      textWithoutTime += `  - ${text}\n`;
+    }
+  }
+  textWithTime += "\n---\n";
+  textWithoutTime += "\n---\n\n";
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [groupName, projectNames] of Object.entries(GROUP_SETTINGS)) {
+    const projects = summary.data.filter((project) => projectNames.includes(project.title.project));
+    const sumOfGroup = projects.map((project) => project.time).reduce((sum, elm) => sum + elm, 0);
+
+    textWithTime += `## ${groupName} の合計時間: ${createTimeText(sumOfGroup, summary.total_grand)}\n`;
+    textWithoutTime += ``;
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const project of projects) {
+      textWithTime += `- ${createTimeText(project.time, summary.total_grand)} ${project.title.project}\n`;
+      textWithoutTime += `- ${project.title.project}\n`;
+      // eslint-disable-next-line no-restricted-syntax
+      for (const item of project.items) {
+        // eslint-disable-next-line no-await-in-loop
+        textWithTime += `  - ${createTimeText(item.time, summary.total_grand)} ${item.title.time_entry}\n`;
+        textWithoutTime += `  - ${item.title.time_entry}\n`;
+      }
+    }
+  }
+
+  let resultText = "";
+  resultText += `${convertDateToString(since)} ~ ${convertDateToString(until)}\n`;
+  resultText += `${textWithTime}`;
+  resultText += "\n------------\n\n";
+  resultText += `${textWithoutTime}`;
+
+  const resultsDirPath = process.env.RESULTS_DIR_ABSOLUTE_PATH;
+
+  const filePath = `${resultsDirPath}/${convertDateToString(since)}_${convertDateToString(until)}.md`;
+  if (!fs.existsSync(resultsDirPath)) {
+    fs.mkdirSync(resultsDirPath, { recursive: true });
+  }
+  fs.writeFileSync(filePath, resultText);
+
+  // eslint-disable-next-line no-console
+  console.log(`🎉結果を保存しました🎉: ${filePath}`);
+};
+
+main();
