4eba83027f095d18ab5fb2b09716bdc6fb537492
Hiroto Hasegawa <hiroto.hasegawa.0828@gmail.com>
2022-10-01 17:01:07 +0900

    remove: api Ver. ができたので、 csv2text が不要になったので削除した

--- src/csv2text/constants.example.ts
+++
@@ -1,9 +0,0 @@
-export const NAME_TO_ALIAS_MAP = new Map<string, string>([
-  ["name", "名前"],
-  ["name2", "名前2"],
-]);
-
-export const GROUP_SETTINGS = {
-  A_GROUP: ["name1", "name2"],
-  B_GROUP: ["name3"],
-};
