090f3552d79490cac411fed91835cf083adb0c1c
Hiroto Hasegawa <hiroto.hasegawa.0828@gmail.com>
2022-10-02 15:58:01 +0900

    feat: api.runTimeEntry を作成した

--- src/common/api.ts
+++ src/common/api.ts
@@ -27,6 +27,30 @@ class Api {
 
     return res.json();
   };
+
+  runTimeEntry = async (description: string, pid: string): Promise<void> => {
+    const bodyJson = {
+      time_entry: {
+        description,
+        pid,
+        created_with: `toggl-tools:${this.workspaceId}`,
+      },
+    };
+    const res = await fetch("https://api.track.toggl.com/api/v8/time_entries/start", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${Buffer.from(`${this.apiToken}:api_token`).toString("base64")}`,
+      },
+      body: JSON.stringify(bodyJson),
+    });
+
+    if (!res.ok) {
+      throw new Error(`Failed to run time entry: ${res.statusText}`);
+    }
+
+    return res.json();
+  };
 }
 
 export default Api;
